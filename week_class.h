2bf872cb62c7788415f61227a9302d026131ddb9
stasiandr <stasiandr@gmail.com>
2015-10-07 11:17:03 +0300

    please check me

--- week_class.h
+++ week_class.h
@@ -3,6 +3,7 @@ using namespace std;
 Class Lesson {
     string lesson_name;
     string teacher_name;
+    Lesson (string l, string t) {lesson_name = l; teacher_name = t;}
 };
 // Прсосто указание урока и имени учителя для последующей обработки
 
@@ -13,5 +14,7 @@ Class Week {
     Lesson thursday[10];
     Lesson friday[10];
     Lesson saturday[10];
+    Week (Lesson mon[10], Lesson tue[10], Lesson wed[10], Lesson thu[10], Lesson fri[10], Lesson sat[10])
+        {monday = mon; tuesday = tue; wednesday = wed; thursday = thu; friday = fri; saturday = sat;}
 };
 // Работаем уже с рссписанием. Расписание для одново класса на неделю. Все рассписание предлагается хранить как массив Week для всех классов
