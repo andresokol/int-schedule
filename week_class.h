823cb7af3cc4cd6a4131dc1cc724bfe4e942eeb1
stasiandr <stasiandr@gmail.com>
2015-10-07 11:06:32 +0300

    initiated Week class

---
+++ week_class.h
@@ -0,0 +1,16 @@
+using namespace std;
+
+Class Lesson {
+    string lesson_name;
+    string teacher_name;
+    int class_year;
+};
+
+Class Week {
+    Lesson monday[10];
+    Lesson tuesday[10];
+    Lesson wednesday[10];
+    Lesson thursday[10];
+    Lesson friday[10];
+    Lesson saturday[10];
+}
